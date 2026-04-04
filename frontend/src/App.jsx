7b9c0d0af534e2ea6bd8b4764fd00c20e52ef359
Paul Ndambo <paulkadabo@gmail.com>
2026-04-04 16:56:04 +0300

    Experimental Landing Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,24 @@
 import { useState } from 'react'
 import './App.css';
+
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import HomePage from './app/pages/HomePage';
 
+import { LandingPage } from './app/pages/LandingPage'
+import { LoginPage } from './app/pages/LoginPage'
+import { RegisterPage } from './app/pages/RegisterPage'
+import { BookingPage } from './app/pages/BookingPage'
 
-export default function App() {
+function App() {
   return (
-    <HomePage />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/booking" element={<BookingPage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
